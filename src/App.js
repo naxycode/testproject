d8835f47895e25dc44a3a708f024eb9de2968143
naxycode <naxycode@mailfence.com>
2020-04-24 06:42:59 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+
+import './assets/default.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div class="row">
+        <div class="columns small-12">
+          <h1 class="text-center">CRUD APP</h1>
+        </div>
+      </div>
+      
     </div>
   );
 }
---
+++ src/testmodule/module.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Module() {
+  return (
+    <div className="module">
+		<h3 class="text-center">Module 21</h3>
+		<p>Разобрался как резать вёрстку на блоки и подрубать свои стили</p>
+    </div>
+  );
+}
+
+export default Module;
