01ffddf7250741681e6a4beaf7120570d01aa8f8
naxycode <naxycode@mailfence.com>
2020-04-24 07:20:18 +0300

    проверка связи

--- src/testmodule/module.js
+++ src/testmodule/module.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
 function Module() {
+
+	function formatName(user) {
+	  return user.firstName + ' ' + user.lastName;
+	}
+
+	const user = {
+	  firstName: 'Тестирование',
+	  lastName: 'React',
+	};
+
+	const element = <h1>Hello, {formatName(user)}!</h1>;
+
+
   return (
-    <div className="module">
-		<h3 class="text-center">Module 21</h3>
+    <div className="module text-center">
+    	{element}
+    	<strong>4*25={4*25}</strong>
+		<h3>Module 21</h3>
 		<p>Разобрался как резать вёрстку на блоки и подрубать свои стили</p>
     </div>
   );
 }
 
 export default Module;
+
+
